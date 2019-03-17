896c44906aa7f176d49e6492a6f2c63c2bba2879
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2019-03-17 15:25:59 +0900

    support multi color

--- highlighter.py
+++ highlighter.py
@@ -1,6 +1,15 @@
 import sublime
 import sublime_plugin
 
+from collections import OrderedDict
+
+COLORS_BY_SCOPE = OrderedDict()
+COLORS_BY_SCOPE['string'] = False
+COLORS_BY_SCOPE['entity.name.class'] = False
+COLORS_BY_SCOPE['variable.parameter'] = False
+COLORS_BY_SCOPE['invalid.deprecated'] = False
+COLORS_BY_SCOPE['invalid'] = False
+COLORS_BY_SCOPE['support.function'] = False
 
 class SelectAreaCommand(sublime_plugin.WindowCommand):
   def run(self):
@@ -11,7 +20,8 @@ class SelectAreaCommand(sublime_plugin.WindowCommand):
     views = self.window.views()
     for view in views:
       regions = find_regexes(view, sel_string)
-      view.add_regions(sel_string, regions, 'keyword', 'circle')
+      color = find_color()
+      view.add_regions(sel_string, regions, color, 'circle')
 
 class EraseHighlightCommand(sublime_plugin.WindowCommand):
   def run(self):
@@ -25,3 +35,13 @@ class EraseHighlightCommand(sublime_plugin.WindowCommand):
 
 def find_regexes(view, sel_string):
   return view.find_all(sel_string)
+
+def find_color():
+  color = ""
+  for key, value in COLORS_BY_SCOPE.items():
+    if value == False:
+      COLORS_BY_SCOPE[key] = True
+      color = key
+      break
+  return color
+
