a3f43b5f2eaa0570260c86f9c4444ba5f887e2c6
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2019-03-21 23:54:38 +0900

    Use global value

--- highlighter.py
+++ highlighter.py
@@ -5,10 +5,6 @@ import re
 from collections import OrderedDict
 
 class TextHighlighterToggleCommand(sublime_plugin.WindowCommand):
-  def __init__(self, window):
-    super().__init__(window)
-    window.set_project_data(init_color())
-
   # TODO: run this command when new tab is opened
   def run(self):
     active_view = self.window.active_view()
@@ -30,45 +26,41 @@ class TextHighlighterToggleCommand(sublime_plugin.WindowCommand):
       for view in views:
         highlighter(view, sel_string, color)
 
-class TextHighlighterClearAllCommand(sublime_plugin.WindowCommand):
-  def __init__(self, window):
-    super().__init__(window)
-    window.set_project_data(init_color())
 
+class TextHighlighterClearAllCommand(sublime_plugin.WindowCommand):
   def run(self):
     window =self.window
     views = self.window.views()
-    colors_by_scope = window.project_data()
+
     for sel_string in colors_by_scope.values():
       if sel_string:
         color = find_used_color(window, sel_string)
         if color:
           for view in views:
               eraser(view, sel_string, color)
-    window.set_project_data(init_color())
 
 
 class HighlighterCommand(sublime_plugin.EventListener):
   def on_modified(self, view):
     window = view.window()
     print('window in on_modified --------------------: ', window)
-    colors_by_scope = window.project_data()
     views = window.views()
     for view in views:
       for color, sel_string in colors_by_scope.items():
         if color and sel_string:
           highlighter(view, sel_string, color)
 
+
 def highlighter(view, sel_string, color):
+  global colors_by_scope
   window = view.window()
-  colors_by_scope = window.project_data()
   print('--------------------- colors_by_scope in highlighter ---------- : ', colors_by_scope)
 
   regions = find_all(view, sel_string)
   if color and regions:
     if not colors_by_scope[color]:
       colors_by_scope[color] = sel_string
-      window.set_project_data(colors_by_scope)
+
     view.add_regions(
       sel_string,
       regions,
@@ -78,19 +70,19 @@ def highlighter(view, sel_string, color):
       )
 
 def eraser(view, sel_string, color):
+  global colors_by_scope
   window = view.window()
-  colors_by_scope = window.project_data()
 
   regions = find_all(view, sel_string)
   colors_by_scope[color] = None
-  window.set_project_data(colors_by_scope)
+
   view.erase_regions(sel_string)
 
 def find_all(view, sel_string):
   return view.find_all(re.escape(sel_string))
 
 def is_highlighted(window, sel_string):
-  colors_by_scope = window.project_data()
+  global colors_by_scope
 
   highlighted = False
   for key, value in colors_by_scope.items():
@@ -100,7 +92,8 @@ def is_highlighted(window, sel_string):
   return highlighted
 
 def find_used_color(window, sel_string):
-  colors_by_scope = window.project_data()
+  global colors_by_scope
+
   color = None
   for key, value in colors_by_scope.items():
     if value == sel_string:
@@ -108,7 +101,8 @@ def find_used_color(window, sel_string):
   return color
 
 def find_usable_color(window, sel_string):
-  colors_by_scope = window.project_data()
+  global colors_by_scope
+
   color = None
   for key, value in colors_by_scope.items():
     if not value:
@@ -116,7 +110,12 @@ def find_usable_color(window, sel_string):
       break
   return color
 
-def init_color():
+def plugin_loaded():
+  global colors_by_scope
+
+  if colors_by_scope: # Return colors_by_scope if colors_by_scope is already exists.
+    return colors_by_scope
+
   colors_by_scope = OrderedDict() # TODO: Make color configurable
   colors_by_scope['markup.changed.git_gutter'] = None # vivid purple
   colors_by_scope['support.class'] = None # yellow
@@ -127,5 +126,7 @@ def init_color():
   colors_by_scope['variable'] = None # red
   colors_by_scope['string'] = None # light green
   colors_by_scope['comment'] = None # glay
+
   return colors_by_scope
 
+plugin_loaded()
